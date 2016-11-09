74e4565fa7b7bba398b17959fc7dd00dab4cdade
ucha <ucha@clips4sale.com>
2016-11-09 14:31:14 +0400

    configured dingo api

--- config/app.php
+++ config/app.php
@@ -177,6 +177,9 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        //Dingo API Package
+        Dingo\Api\Provider\LaravelServiceProvider::class
+
 
     ],
 
