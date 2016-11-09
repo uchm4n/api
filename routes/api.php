a5f1bc52deac686451f5c2b484618cb0dcf34cbd
ucha <ucha@clips4sale.com>
2016-11-09 15:18:24 +0400

    added Testing Endpoint

--- routes/api.php
+++ routes/api.php
@@ -1,21 +1,12 @@
 <?php
-
-use Illuminate\Http\Request;
-
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+//Registre Dingo PI Router
 $api = app('Dingo\Api\Routing\Router');
 
-$api->version('v1', function ($api) {
+
+//Endpoints
+$api->version('v1',['namespace' => 'App\Http\Controllers'], function ($api) {
+    $api->get('/test','Test@test');
     $api->group(['middleware' => 'auth:api'], function ($api) {
-        $api->get('/test','App\Controllers\Test@test');
+        $api->get('/testing','Test@test');
     });
 });
