12f0897040587dc6b4d54351f909efaeecbe9ef2
ucha <ucha@clips4sale.com>
2016-11-13 17:38:23 +0400

    set migration default value

--- database/migrations/2016_11_13_095022_create_tasks_table.php
+++ database/migrations/2016_11_13_095022_create_tasks_table.php
@@ -17,7 +17,7 @@ class CreateTasksTable extends Migration
             $table->increments('id');
             $table->string('title');
             $table->integer('user_id');
-            $table->boolean('completed');
+            $table->boolean('completed')->default(0);
             $table->timestamps();
         });
     }
