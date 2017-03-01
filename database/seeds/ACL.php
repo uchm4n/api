60791ee279f4930cb4c3cd8e1c13786d0c717daf
ucha19871 <ucha19871@gmail.com>
2017-03-01 20:02:19 +0400

    fix seeders

--- database/seeds/ACL.php
+++ database/seeds/ACL.php
@@ -14,7 +14,7 @@ class ACL extends Seeder
      */
     public function run()
     {
-        $user = new User();
+        $user = new App\User();
         $user = $user->where('email','ucha19871@gmail.com')->firstOrFail();
 
         $owner = new Role();
@@ -39,8 +39,8 @@ class ACL extends Seeder
         $dashboard->description  = 'User dashboard access permission'; // optional
         $dashboard->save();
 
+        $owner->attachPermission($dashboard);
         $admin->attachPermission($dashboard);
-        $owner->attachPermissions($dashboard);
 
         return [
             $user->hasRole('owner'),   // true
--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\User;
 use Illuminate\Database\Seeder;
 
 class UsersTableSeeder extends Seeder
@@ -11,7 +12,7 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        $user = new \App\User();
+        $user = new User();
         $user->name = 'Ucha';
         $user->email = 'ucha19871@gmail.com';
         $user->password = '12345';
@@ -19,6 +20,6 @@ class UsersTableSeeder extends Seeder
         $user->bio = 'Something Something Info';
         $user->image = 'http://placehold.it/32x32';
         $user->save();
-        factory(App\User::class, 30)->create();
+        factory(User::class, 30)->create();
     }
 }
