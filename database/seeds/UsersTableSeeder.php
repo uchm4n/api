58ad4049808c898b702345c98e0395e061630887
ucha19871 <ucha19871@gmail.com>
2017-03-01 22:02:06 +0400

    seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -13,11 +13,11 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         $user = new User();
-        $user->name = 'Ucha';
-        $user->email = 'ucha19871@gmail.com';
+        $user->name = 'Demo';
+        $user->email = 'demo@example.com';
         $user->password = '12345';
         $user->phone = '2342342342';
-        $user->bio = 'Something Something Info';
+        $user->bio = 'Something Something Info...';
         $user->image = 'http://placehold.it/32x32';
         $user->save();
         factory(User::class, 30)->create();
